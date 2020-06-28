71dc0c8ae5434c07fbd63d76381cb5f9671898a8
laofuLF <weihuanf@uci.edu>
2020-06-27 20:09:38 -0700

    added gitignore

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
     constructor(props) {
@@ -20,6 +21,19 @@ class TodoItem extends Component {
         const { handleItemDelete, index } = this.props;
         handleItemDelete(index);
     }
+}
+
+// check prop types
+// will throw warning if type does not match
+TodoItem.protoTypes = {
+    content: PropTypes.string.isRequired,
+    handleItemDelete: PropTypes.func,
+    index: PropTypes.number
+}
+
+// assign default value if not received
+TodoItem.defaultProps = {
+    test: 'value1'
 }
 
 export default TodoItem;
