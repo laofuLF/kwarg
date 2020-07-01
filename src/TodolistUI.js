04dc6c75132c8581272e370c55fb0ffac06a3156
laofuLF <weihuanf@uci.edu>
2020-06-30 19:24:28 -0700

    stateless component used
    use stateless component if it only has UI function

--- src/TodolistUI.js
+++ src/TodolistUI.js
@@ -1,34 +1,32 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Button, Divider, Input, List, Typography} from "antd";
 
-class TodolistUI extends Component {
-    render() {
-        return (
+const TodolistUI = (props) => {
+    return (
+        <div>
             <div>
-                <div>
-                    <Input
-                        placeholder='add task'
-                        value={this.props.inputValue}
-                        style={{width:400, marginRight:25}}
-                        onChange={this.props.handleInputChange}
-                    />
-                    <Button type='default' onClick={this.props.handleButtonClick}>add</Button>
-                </div>
-                <Divider orientation="left">Default Size</Divider>
-                <List
-                    style={{marginTop:10, width:400}}
-                    dataSource={this.props.list}
-                    renderItem={(item, index) => (
-                        <List.Item
-                            onClick={(index) => {this.props.handleItemClick(index)}}
-                        >
-                            <Typography.Text mark/> {item}
-                        </List.Item>
-                    )}
+                <Input
+                    placeholder='add task'
+                    value={props.inputValue}
+                    style={{width:400, marginRight:25}}
+                    onChange={props.handleInputChange}
                 />
+                <Button type='default' onClick={props.handleButtonClick}>add</Button>
             </div>
-        )
-    }
+            <Divider orientation="left">Default Size</Divider>
+            <List
+                style={{marginTop:10, width:400}}
+                dataSource={props.list}
+                renderItem={(item, index) => (
+                    <List.Item
+                        onClick={(index) => {props.handleItemClick(index)}}
+                    >
+                        <Typography.Text mark/> {item}
+                    </List.Item>
+                )}
+            />
+        </div>
+    )
 }
 
 export default TodolistUI;
